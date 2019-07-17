3a699d5672b4089a31f687996dce23b5098dfe11
Simon Bürger <simon.buerger@rwth-aachen.de>
2019-07-17 16:17:49 +0200

    add IntHistogram

--- src/util/stats.h
+++ src/util/stats.h
@@ -63,6 +63,56 @@ struct Histogram
 	void add(double x);
 };
 
+class IntHistogram
+{
+	int min_ = 0, max_ = 0;
+	std::vector<size_t> bins_;
+	size_t underflow_ = 0, overflow_ = 0;
+	size_t count_ = 0;
+	double sum_ = 0.0;
+
+  public:
+	IntHistogram() = default;
+	IntHistogram(int min, int max) : min_(min), max_(max), bins_(max - min, 0)
+	{}
+
+	void add(int x)
+	{
+		if (x < min_)
+			underflow_ += 1;
+		else if (x >= max_)
+			overflow_ += 1;
+		else
+			bins_[x - min_] += 1;
+		count_ += 1;
+		sum_ += x;
+	}
+
+	int min() const { return min_; }
+	int max() const { return max_; }
+
+	size_t overflow() const { return overflow_; }
+	size_t underflow() const { return underflow_; }
+	size_t count() const { return count_; }
+	size_t count(int x) const
+	{
+		assert(min_ <= x && x < max_);
+		return bins_[x - min_];
+	}
+	double sum() const { return sum_; }
+	double mean() const { return (double)sum_ / count_; }
+
+	void clear()
+	{
+		for (auto &c : bins_)
+			c = 0;
+		underflow_ = 0;
+		overflow_ = 0;
+		count_ = 0;
+		sum_ = 0;
+	}
+};
+
 /**
  * Estimate mean/variance/covariance of a population as samples are coming in.
  * This is the same as the standard formula "Var(x) = n/(n-1) (E(x^2) - E(x)^2)"

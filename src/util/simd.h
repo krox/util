3d022668d5f5580f088c115728ad5736b6491a11
Simon Bürger <simon.buerger@rwth-aachen.de>
2021-10-22 23:19:17 +0200

    alternative simd implementation using GCC builtins

--- src/util/simd.h
+++ src/util/simd.h
@@ -19,6 +19,7 @@ namespace util {
  *     everything is inlined as expected. Also beware of aliasing issues.
  */
 
+#define UTIL_SIMD_GCC_BUILTIN
 // static constexpr size_t simd_register_size = 16; // 128 bit (SSE)
 static constexpr size_t simd_register_size = 32; // 256 bit (AVX, AVX2)
 // static constexpr size_t simd_register_size = 64; // 512 bit (AVX512)
@@ -26,6 +27,127 @@ static constexpr size_t simd_register_size = 32; // 256 bit (AVX, AVX2)
 using std::sin, std::cos, std::tan, std::exp, std::log, std::sqrt;
 
 #define UTIL_SIMD_FOR(i, w) for (size_t i = 0; i < w; ++i)
+
+#ifdef UTIL_SIMD_GCC_BUILTIN
+
+template <typename T, size_t W = simd_register_size / sizeof(T)>
+struct alignas(sizeof(T) * W) simd
+{
+	static_assert(W > 0 && (W & (W - 1)) == 0); // W has to be a power of two
+
+	typedef T Vec_ __attribute__((vector_size(W * sizeof(T))));
+	Vec_ v_;
+
+	simd() = default;
+
+	simd(T v) { UTIL_SIMD_FOR(i, W) v_[i] = v; }
+
+	T &operator[](size_t i) { return v_[i]; }
+	T operator[](size_t i) const { return v_[i]; }
+};
+
+template <typename T, size_t W> simd<T, W> operator+(simd<T, W> a)
+{
+	simd<T, W> b;
+	b.v_ = +a.v_;
+	return b;
+}
+template <typename T, size_t W> simd<T, W> operator-(simd<T, W> a)
+{
+	simd<T, W> b;
+	b.v_ = -a.v_;
+	return b;
+}
+
+#define UTIL_DEFINE_SIMD_OPERATOR(op)                                          \
+	template <typename T, typename U, size_t W>                                \
+	auto operator op(simd<T, W> a, simd<U, W> b)                               \
+	    ->simd<decltype(a[0] op b[0]), W>                                      \
+	{                                                                          \
+		simd<decltype(a[0] op b[0]), W> c;                                     \
+		c.v_ = a.v_ op b.v_;                                                   \
+		return c;                                                              \
+	}                                                                          \
+	template <typename T, size_t W> simd<T, W> operator op(simd<T, W> a, T b)  \
+	{                                                                          \
+		simd<T, W> c;                                                          \
+		c.v_ = a.v_ op b;                                                      \
+		return c;                                                              \
+	}                                                                          \
+	template <typename T, size_t W> simd<T, W> operator op(T a, simd<T, W> b)  \
+	{                                                                          \
+		simd<T, W> c;                                                          \
+		c.v_ = a op b.v_;                                                      \
+		return c;                                                              \
+	}                                                                          \
+	template <typename T, typename U, size_t W>                                \
+	simd<T, W> &operator op##=(simd<T, W> &a, simd<U, W> b)                    \
+	{                                                                          \
+		a.v_ op## = b.v_;                                                      \
+		return a;                                                              \
+	}                                                                          \
+	template <typename T, typename U, size_t W>                                \
+	simd<T, W> &operator op##=(simd<T, W> &a, U b)                             \
+	{                                                                          \
+		a.v_ op## = b;                                                         \
+		return a;                                                              \
+	}
+
+#define UTIL_DEFINE_SIMD_FUNCTION(fun)                                         \
+	template <typename T, size_t W> simd<T, W> fun(simd<T, W> a)               \
+	{                                                                          \
+		simd<T, W> b;                                                          \
+		UTIL_SIMD_FOR(i, W) b[i] = fun(a[i]);                                  \
+		return b;                                                              \
+	}
+
+UTIL_DEFINE_SIMD_OPERATOR(+)
+UTIL_DEFINE_SIMD_OPERATOR(-)
+UTIL_DEFINE_SIMD_OPERATOR(*)
+UTIL_DEFINE_SIMD_OPERATOR(/)
+
+UTIL_DEFINE_SIMD_FUNCTION(sin)
+UTIL_DEFINE_SIMD_FUNCTION(cos)
+UTIL_DEFINE_SIMD_FUNCTION(tan)
+UTIL_DEFINE_SIMD_FUNCTION(exp)
+UTIL_DEFINE_SIMD_FUNCTION(log)
+UTIL_DEFINE_SIMD_FUNCTION(sqrt)
+
+#undef UTIL_DEFINE_SIMD_OPERATOR
+#undef UTIL_DEFINE_SIMD_FUNCTION
+#undef UTIL_SIMD_FOR
+
+// operations that dont act independently accross SIMD lanes
+//     * prefixed with 'v' in order not to clash with parallel instructions
+//     * can be overloaded for horizontal SIMD i.e. things like
+//         vsum(Matrix<simd<double>>) -> Matrix<double>
+
+template <typename T, size_t W> T vsum(simd<T, W> a)
+{
+	T r = a[0];
+	for (size_t i = 1; i < W; ++i)
+		r += a[i];
+	return r;
+}
+template <typename T, size_t W>
+simd<T, W> vshuffle(simd<T, W> a, simd<int, W> mask)
+{
+	simd<T, W> r;
+	for (size_t i = 0; i < W; ++i)
+		r[i] = a[mask[i]];
+	return r;
+}
+template <typename T, size_t W> T vextract(simd<T, W> a, size_t lane)
+{
+	// assert(i < W);
+	return a[lane];
+}
+template <typename T, size_t W> void vinsert(simd<T, W> &a, size_t lane, T b)
+{
+	a[lane] = b;
+}
+
+#else
 
 template <typename T, size_t W = simd_register_size / sizeof(T)>
 struct alignas(sizeof(T) * W) simd
@@ -141,5 +263,7 @@ template <typename T, size_t W> void vinsert(simd<T, W> &a, size_t lane, T b)
 {
 	a[lane] = b;
 }
+
+#endif
 
 } // namespace util

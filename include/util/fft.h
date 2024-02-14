757b80566103a2f024939ca54048345cdd42e775
Simon Bürger <simon.buerger@rwth-aachen.de>
2024-02-14 11:59:55 +0000

    more convenient FFT(W) interface

--- include/util/fft.h
+++ include/util/fft.h
@@ -1,7 +1,18 @@
 #pragma once
 
 // simple wrapper around the FFTW3 library
-
+// general notes on FFTW:
+//     * For best performance, arrays should be highly aligned
+//       (e.g., using util::aligned_allocate<util::complex<double>>(...)).
+//     * Performance is best for sizes that are products of small primes,
+//       especially pure powers of 2. Though FFTW uses O(n log n) algorithms
+//       even for large prime sizes.
+//     * 'sign' is the sign in the 'exp(...)' part of the transform. The
+//       constants FFTW_FORWARD = -1 and FFTW_BACKWARD = 1 are just convention.
+//     * By default, some types of transform destroy the input array. This can
+//       be controlled using FFTW_{PRESERVE,DESTROY}_INPUT.
+//     * FFTW computes 'unnormalized' transforms, i.e., a forward transform
+//       followed by a backward transform will multiply the input by n.
 #ifdef UTIL_FFTW
 
 #include "util/complex.h"
@@ -24,29 +35,49 @@ struct fftw_plan_delete
 using unique_fftw_plan =
     std::unique_ptr<std::remove_pointer_t<fftw_plan>, fftw_plan_delete>;
 
-// thin wrappers around fftw_plan_*, mostly to adapt types a little bit
+// basic wrappers around fftw_plan_*
 //     * throws on errors instad of returning nullptr
-//     * FFTW_FORWARD = -1, FFTW_BACKWARD = 1, though this is just convention
-//     * by default, planning destoys both input and output
-//     * for optimal performance, arrays should be aligned, for example using
-//           util::aligned_allocate<util::complex<double>>(...)
+//     * In contrast to the raw FFTW routines, these planning functions do not
+//       destroy the input array (more precisely, they restore it after
+//       planning).The output array is not saved though.
 
-// 1D transform
-unique_fftw_plan plan_fft(int n, util::complex<double> *in,
-                          util::complex<double> *out, int sign,
-                          int flags = FFTW_MEASURE);
+// 1d complex-to-complex
+unique_fftw_plan plan_fft_1d(int n, util::complex<double> *in,
+                             util::complex<double> *out, int sign,
+                             int flags = FFTW_MEASURE);
 
-// transforms all axes
+// 1d complex-to-real-to-complex. the real array should be of size n/2+1
+unique_fftw_plan plan_fft_r2c_1d(int n, double *in, util::complex<double> *out,
+                                 int flags = FFTW_MEASURE);
+unique_fftw_plan plan_fft_c2r_1d(int n, util::complex<double> *in, double *out,
+                                 int flags = FFTW_MEASURE);
+
+// transforms all axes of multi-dimensional array (assumed to be row-major)
 unique_fftw_plan plan_fft_all(std::span<const int> shape,
                               util::complex<double> *in,
                               util::complex<double> *out, int sign,
                               int flags = FFTW_MEASURE);
 
-// transforms just the last axis
-unique_fftw_plan plan_fft_last(std::span<const int> shape,
-                               util::complex<double> *in,
-                               util::complex<double> *out, int sign,
-                               int flags = FFTW_MEASURE);
+// higher-level interface for convenience that hides FFTW's 'planning' stage
+//     * thanks to FFTW's internal wisdom collection, planning should only be
+//       expensive at most once for any given shape/type/flags/alignment
+//       combination, but the overhead might still be a bit higher than creating
+//       and (re-)using a plan explicitly.
+
+void fft_1d(std::span<util::complex<double>> in,
+            std::span<util::complex<double>> out, int sign,
+            int flags = FFTW_MEASURE);
+void fft_1d(std::span<util::complex<double>> inout, int sign,
+            int flags = FFTW_MEASURE);
+void fft_r2c_1d(std::span<double> in, std::span<util::complex<double>> out,
+                int flags = FFTW_MEASURE);
+void fft_c2r_1d(std::span<util::complex<double>> in, std::span<double> out,
+                int flags = FFTW_MEASURE);
+void fft_all(std::span<const int> shape, std::span<util::complex<double>> in,
+             std::span<util::complex<double>> out, int sign,
+             int flags = FFTW_MEASURE);
+void fft_all(std::span<const int> shape, std::span<util::complex<double>> inout,
+             int sign, int flags = FFTW_MEASURE);
 
 } // namespace util
 
--- src/util/fft.cpp
+++ src/util/fft.cpp
@@ -6,43 +6,131 @@
 
 namespace util {
 
-unique_fftw_plan plan_fft(int n, util::complex<double> *in,
-                          util::complex<double> *out, int sign, int flags)
+namespace {
+template <class T> struct Stash
+{
+	T *ptr_;
+	util::unique_memory<T> stash_;
+	explicit Stash(T *ptr, size_t n) : ptr_(ptr), stash_(util::allocate<T>(n))
+	{
+		std::copy_n(ptr_, stash_.size(), stash_.data());
+	}
+	~Stash() { std::copy_n(stash_.data(), stash_.size(), ptr_); };
+};
+} // namespace
+
+unique_fftw_plan plan_fft_1d(int n, util::complex<double> *in,
+                             util::complex<double> *out, int sign, int flags)
 {
 	auto plan = fftw_plan_dft_1d(n, (fftw_complex *)in, (fftw_complex *)out,
-	                             sign, flags);
+	                             sign, flags | FFTW_WISDOM_ONLY);
 	if (!plan)
-		throw std::runtime_error("fftw_plan_dft_1d() failed");
-	return unique_fftw_plan(plan);
-}
-
-unique_fftw_plan plan_fft_all(std::span<const int> const shape,
-                              util::complex<double> *in,
-                              util::complex<double> *out, int sign, int flags)
-{
-	auto plan = fftw_plan_dft(shape.size(), shape.data(), (fftw_complex *)in,
-	                          (fftw_complex *)out, sign, flags);
+	{
+		auto stash = Stash(in, n);
+		plan = fftw_plan_dft_1d(n, (fftw_complex *)in, (fftw_complex *)out,
+		                        sign, flags);
+	}
 	if (!plan)
 		throw std::runtime_error("fftw_plan_dft() failed");
 	return unique_fftw_plan(plan);
 }
 
-unique_fftw_plan plan_fft_last(std::span<const int> shape,
-                               util::complex<double> *in,
-                               util::complex<double> *out, int sign, int flags)
+unique_fftw_plan plan_fft_r2c_1d(int n, double *in, util::complex<double> *out,
+                                 int flags)
 {
-	assert(shape.size() >= 1);
-	int howmany = 1; // number of independent FFTs
-	for (size_t i = 0; i < shape.size() - 1; ++i)
-		howmany *= shape[i];
-	int dist = shape.back(); // distance from one FFT to the next
-	int stride = 1;          // stride inside one FFT
-	auto plan = fftw_plan_many_dft(
-	    1, &shape.back(), howmany, (fftw_complex *)in, nullptr, stride, dist,
-	    (fftw_complex *)out, nullptr, stride, dist, sign, flags);
+	auto plan = fftw_plan_dft_r2c_1d(n, in, (fftw_complex *)out,
+	                                 flags | FFTW_WISDOM_ONLY);
 	if (!plan)
-		throw std::runtime_error("fftw_plan_many_dft() failed");
+	{
+		auto stash = Stash(in, n);
+		plan = fftw_plan_dft_r2c_1d(n, in, (fftw_complex *)out, flags);
+	}
+	if (!plan)
+		throw std::runtime_error("fftw_plan_dft_r2c() failed");
 	return unique_fftw_plan(plan);
+}
+
+unique_fftw_plan plan_fft_c2r_1d(int n, util::complex<double> *in, double *out,
+                                 int flags)
+{
+	auto plan = fftw_plan_dft_c2r_1d(n, (fftw_complex *)in, out,
+	                                 flags | FFTW_WISDOM_ONLY);
+	if (!plan)
+	{
+		auto stash = Stash(in, n / 2 + 1);
+		plan = fftw_plan_dft_c2r_1d(n, (fftw_complex *)in, out, flags);
+	}
+	if (!plan)
+		throw std::runtime_error("fftw_plan_dft_c2r() failed");
+	return unique_fftw_plan(plan);
+}
+
+unique_fftw_plan plan_fft_all(std::span<const int> shape,
+                              util::complex<double> *in,
+                              util::complex<double> *out, int sign, int flags)
+{
+	auto plan =
+	    fftw_plan_dft((int)shape.size(), shape.data(), (fftw_complex *)in,
+	                  (fftw_complex *)out, sign, flags | FFTW_WISDOM_ONLY);
+	if (!plan)
+	{
+		size_t n = std::accumulate(shape.begin(), shape.end(), size_t(1),
+		                           std::multiplies<size_t>());
+		auto stash = Stash(in, n);
+		plan =
+		    fftw_plan_dft((int)shape.size(), shape.data(), (fftw_complex *)in,
+		                  (fftw_complex *)out, sign, flags);
+	}
+	if (!plan)
+		throw std::runtime_error("fftw_plan_dft() failed");
+	return unique_fftw_plan(plan);
+}
+
+void fft_1d(std::span<util::complex<double>> in,
+            std::span<util::complex<double>> out, int sign, int flags)
+{
+	assert(in.size() == out.size());
+	assert(in.size() <= INT_MAX);
+	auto p = plan_fft_1d((int)in.size(), in.data(), out.data(), sign, flags);
+	fftw_execute(p.get());
+}
+void fft_1d(std::span<util::complex<double>> inout, int sign, int flags)
+{
+	fft_1d(inout, inout, sign, flags);
+}
+
+void fft_r2c_1d(std::span<double> in, std::span<util::complex<double>> out,
+                int flags)
+{
+	assert(in.size() / 2 + 1 == out.size());
+	assert(in.size() <= INT_MAX);
+	auto p = plan_fft_r2c_1d((int)in.size(), in.data(), out.data(), flags);
+	fftw_execute(p.get());
+}
+
+void fft_c2r_1d(std::span<util::complex<double>> in, std::span<double> out,
+                int flags)
+{
+	assert(in.size() == out.size() / 2 + 1);
+	assert(out.size() <= INT_MAX);
+	auto p = plan_fft_c2r_1d((int)out.size(), in.data(), out.data(), flags);
+	fftw_execute(p.get());
+}
+
+void fft_all(std::span<const int> shape, std::span<util::complex<double>> in,
+             std::span<util::complex<double>> out, int sign, int flags)
+{
+	size_t n = std::accumulate(shape.begin(), shape.end(), size_t(1),
+	                           std::multiplies<size_t>());
+	assert(in.size() == n);
+	assert(out.size() == n);
+	auto p = plan_fft_all(shape, in.data(), out.data(), sign, flags);
+	fftw_execute(p.get());
+}
+void fft_all(std::span<const int> shape, std::span<util::complex<double>> inout,
+             int sign, int flags)
+{
+	fft_all(shape, inout, inout, sign, flags);
 }
 
 } // namespace util
